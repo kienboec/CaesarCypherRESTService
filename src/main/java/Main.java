87b150d23d2345840c233d18ea238fdac502a60b
kienboeck <kienboec@technikum-wien.at>
2021-05-04 11:33:17 +0200

    infra initialized with Controller prepared for further implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,32 @@
+import com.sun.net.httpserver.HttpServer;
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(GetWelcomeString());
+
+        ResourceConfig rc = new ResourceConfig()
+                .packages("at.kienboeck.swlcm.cypher");
+
+        HttpServer server = JdkHttpServerFactory.createHttpServer(
+                URI.create("http://localhost:8080/"), rc);
+        System.out.println("Server started");
+
+        // allow access to network to provide server (firewall)
+
+        try {
+            System.out.println("Press enter to close the server");
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        server.stop(0);
+
     }
 
     public static String GetWelcomeString(){
